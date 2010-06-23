da58094b9b74214332541ddb099d121cdce9b01b
tarcisio <sassara.tarcisio@gmail.com>
2010-06-23 20:47:58 -0300

    adicionado configurações do componente Security

--- controllers/roles_controller.php
+++ controllers/roles_controller.php
@@ -2,15 +2,41 @@
 
 class RolesController extends AppController
 {
-	// TODO: remover scaffold
-	public $scaffold;
+	/**
+	 * Antes de executar uma ação
+	 * 
+	 * @return void
+	 * @access public
+	 */
+	public function beforeFilter() {
+		// permite deletar um cargo apenas usando o metodo POST
+		$this->Security->requirePost('delete');
+		
+		// protege o formulário de adição
+		$this->Security->requireAuth('add');
+	}
 	
+	
+	/**
+	 * Lista os cargos cadastrados
+	 * 
+	 * @return void
+	 * @access public
+	 */
 	public function index()
 	{
 		$roles = $this->paginate('Role');
 		$this->set('roles', $roles);
 	}
 	
+	
+	/**
+	 * Visualiza um cargo com sua descrição
+	 * 
+	 * @param int $id O id da role que será mostrada
+	 * @return void
+	 * @access public
+	 */
 	public function view($id = null)
 	{
 		$options = array(
@@ -22,6 +48,22 @@ class RolesController extends AppController
 		$role = $this->Role->find('first', $options);
 		
 		$this->set('role', $role);
+	}
+	
+	
+	/**
+	 * Adiciona um novo cargo
+	 * 
+	 * @return void
+	 * @access public
+	 */
+	public function add()
+	{
+		if ($this->RequestHandler->isPost()) {
+			if ($this->Role->save($this->data['Role'])) {
+				$this->redirect('/roles');
+			}
+		}
 	}
 }
 
